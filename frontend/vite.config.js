b13c7574da53b5e43022a4fa5a781465daedbedd
kashishgfuturetech-eng <kashish.p18033@gmail.com>
2026-04-10 13:00:27 +0530

    allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,16 +6,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    open: true,
-    allowedHosts: [
-      'predly-api.onrender.com',
-      'all'],
+    allowedHosts: 'all',
     proxy: {
       // Dev only: proxies /api → local Flask on 5001
       '/api': {
         target: 'http://localhost:5001',
         changeOrigin: true,
-        secure: false
       },
     },
   },
